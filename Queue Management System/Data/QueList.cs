c462639fecc7a095b1dcc0f5de40b68204edecc7
MICHAEL MATHU <mathumichael17@gmail.com>
2023-01-30 14:41:56 +0300

    Added QueList, ServicePoint and User Entity Models

---
+++ Queue Management System/Data/QueList.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Queue_Management_System.Data
+{
+    public class QueList
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("ServicePointId")]
+        public ServicePoint ServicePoint { get; set; }
+        public int ServicePointId { get; set; }
+    }
+}
---
+++ Queue Management System/Data/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Queue_Management_System.Data
+{
+    public class User
+    {
+        [Key]
+        public int Id{ get; set; }
+        public string Name{ get; set; }
+        public int Password{ get; set; }
+    }
+}
