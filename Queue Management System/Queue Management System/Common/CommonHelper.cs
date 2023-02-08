f598546c5356cec78e6be65ce2fac9a8e38c6707
MICHAEL MATHU <mathumichael17@gmail.com>
2023-02-08 13:17:21 +0300

    Added Common helper file

---
+++ Queue Management System/Queue Management System/Common/CommonHelper.cs
@@ -0,0 +1,41 @@
+﻿using Npgsql;
+using Queue_Management_System.Data;
+
+namespace Queue_Management_System.Common
+{
+    public class CommonHelper
+    {
+        private IConfiguration _config;
+        public CommonHelper(IConfiguration config)
+        {
+            _config = config;
+        }
+        public User GetUserByUserName(string query)
+        {
+            User user = new User();
+
+            string connectionString = _config.GetConnectionString("DefaultConnection");
+
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = query;
+                NpgsqlCommand command = new NpgsqlCommand(sql, connection);
+                using (NpgsqlDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        user.Id = Convert.ToInt32(dataReader["id"]);
+                        user.Name = dataReader["name"].ToString();
+                        user.Password =dataReader["password"].ToString();
+                        user.Role = dataReader["role"].ToString();
+                        user.ServicePointId = Convert.ToInt32(dataReader["servicepointid"]);
+                    }
+                }
+                connection.Close();
+            }
+            return user;
+        }
+    }
+}
