4afeb0f0ee91e183277cc84900b09d90fc3ee813
MICHAEL MATHU <mathumichael17@gmail.com>
2023-02-08 13:13:56 +0300

    Added Data Models

---
+++ Queue Management System/Queue Management System/Data/Queue.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Queue_Management_System.Data
+{
+    public class Queue
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("ServicePointId")]
+        public ServicePoint ServicePoint { get; set; }
+        public int ServicePointId { get; set; }
+        public int Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime CompletedAt { get; set; }
+    }
+}
---
+++ Queue Management System/Queue Management System/Data/ServicePoint.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Queue_Management_System.Data
+{
+    public class ServicePoint
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("ServiceProviderId")]
+        public User User { get; set; }
+        public int ServiceProviderId { get; set; }
+    }
+}
