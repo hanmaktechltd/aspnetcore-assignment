4f2ba223ccc39d36e7f579692f6b29baa2ca878e
coxmd <coxmusyoki@gmail.com>
2023-03-24 10:06:13 +0300

    Added the WaitingController

---
+++ Queue Management System/Queue Management System/Controllers/WaitingController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Queue_Management_System.Models;
+using Queue_Management_System.Models.Data;
+
+public class WaitingController : Controller
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public WaitingController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public IActionResult Waiting(int servicePointId)
+    {
+        var waitingCustomers = _dbContext.Customers
+                .Where(c => !c.IsCalled && c.Status == "Waiting")
+                .ToList();
+
+        if (waitingCustomers == null)
+        {
+            return View("Empty");
+        }
+
+        return View(waitingCustomers);
+    }
+
+}
