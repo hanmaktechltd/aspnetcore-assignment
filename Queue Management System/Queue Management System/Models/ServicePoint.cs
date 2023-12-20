e06820f92292a0a1cd99b0b7aa47a6c8aed055d0
23po <ckodalo@gmail.com>
2023-12-20 10:49:33 +0300

    adds Ticket and ServicePoint relatonship

--- Queue Management System/Queue Management System/Models/ServicePoint.cs
+++ Queue Management System/Queue Management System/Models/ServicePoint.cs
@@ -1,9 +1,10 @@
 
+namespace Queue_Management_System.Models;
 public class ServicePoint {
 
     public int ServicePointID { get; set; }
-    public string Name { get; set; }
+    public string ServicePointName { get; set; }
     public string Description { get; set; }
 
-    
+    public ICollection<Ticket> Tickets { get; set; }
 }
--- Queue Management System/Queue Management System/Models/Ticket.cs
+++ Queue Management System/Queue Management System/Models/Ticket.cs
@@ -1,10 +1,14 @@
 
+namespace Queue_Management_System.Models;
 public class Ticket {
 
     public int TicketId {get; set;}
-    public int Number {get; set;}
+    public int TicketNumber {get; set;}
 
+    public int ServicePointId { get; set; } 
     public DateTime IssueTime {get; set;}
 
     public DateTime ServiceTime {get; set;}
+
+    public ServicePoint ServicePoint { get; set; }
 }
