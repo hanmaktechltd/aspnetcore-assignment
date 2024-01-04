965adbebd8027454679dc6883611722df39f2be7
23po <ckodalo@gmail.com>
2024-01-05 01:10:12 +0300

    Adds Method to Generate Avg Service Time per ServiceProvider

--- Queue Management System/Queue Management System/Services/ReportService.cs
+++ Queue Management System/Queue Management System/Services/ReportService.cs
@@ -22,62 +22,93 @@ public class ReportService : IReportService
     }
 
     public byte[] GenerateReport()
+{
+    try
     {
-        try
+        Report report = new Report();
+        ReportPage page = new ReportPage();
+        page.Name = "ReportPage1";
+        page.CreateUniqueName();
+        page.PaperWidth = 100;
+        page.PaperHeight = 100;
+        report.Pages.Add(page);
+
+        DataBand dataBand = new DataBand();
+        dataBand.Name = "Data1";
+        dataBand.Height = Units.Millimeters * 10;
+        page.Bands.Add(dataBand);
+
+        int totalTicketsIssued = FindTotalTicketsIssued();
+        DisplayTextObject(dataBand, "TotalTicketsLabel", 0, 0, Units.Millimeters * 50, Units.Millimeters * 5, "Total Tickets Issued:");
+        DisplayTextObject(dataBand, "TotalTicketsData", Units.Millimeters * 50, 0, Units.Millimeters * 50, Units.Millimeters * 5, totalTicketsIssued.ToString());
+
+        DisplayTextObject(dataBand, "AverageTimesHeadline", 0, dataBand.Height, Units.Millimeters * 100, Units.Millimeters * 5, "AVG Waiting Times:", 8, FontStyle.Bold);
+        dataBand.Height += Units.Millimeters * 5;
+
+        Dictionary<string, double> averageWaitingTimes = FindAverageWaitingTimePerServicePoint();
+        foreach (var kvp in averageWaitingTimes)
         {
-            Report report = new Report();
-            ReportPage page = new ReportPage();
-            page.Name = "ReportPage1";
-            page.CreateUniqueName();
-            page.PaperWidth = 100;
-            page.PaperHeight = 100;
-            report.Pages.Add(page);
+            string servicePointName = kvp.Key;
+            double avgWaitTimeSeconds = kvp.Value;
 
-            DataBand dataBand = new DataBand();
-            dataBand.Name = "Data1";
-            dataBand.Height = Units.Millimeters * 10;
-            page.Bands.Add(dataBand);
+            int minutes = (int)avgWaitTimeSeconds / 60;
+            int seconds = (int)avgWaitTimeSeconds % 60;
 
-            int totalTicketsIssued = FindTotalTicketsIssued();
-            DisplayTextObject(dataBand, "TotalTicketsLabel", 0, 0, Units.Millimeters * 50, Units.Millimeters * 5, "Total Tickets Issued:");
-            DisplayTextObject(dataBand, "TotalTicketsData", Units.Millimeters * 50, 0, Units.Millimeters * 50, Units.Millimeters * 5, totalTicketsIssued.ToString());
+            string formattedAvgWaitTime = $"{minutes} min {seconds} sec";
 
-            DisplayTextObject(dataBand, "AverageTimesHeadline", 0, dataBand.Height, Units.Millimeters * 100, Units.Millimeters * 5, "AVG Waiting Times:", 8, FontStyle.Bold);
-            dataBand.Height += Units.Millimeters * 5;
+            DisplayTextObject(dataBand, $"{servicePointName}Name", 0, dataBand.Height, Units.Millimeters * 50, Units.Millimeters * 5, servicePointName);
+            DisplayTextObject(dataBand, $"{servicePointName}AvgWaitTime", Units.Millimeters * 50, dataBand.Height, Units.Millimeters * 50, Units.Millimeters * 5, formattedAvgWaitTime);
 
-            Dictionary<string, double> averageWaitingTimes = FindAverageWaitingTimePerServicePoint();
-            foreach (var kvp in averageWaitingTimes)
+            dataBand.Height += Units.Millimeters * 10;
+        }
+
+       
+        DisplayTextObject(dataBand, "AverageServiceTimeHeadline", 0, dataBand.Height, Units.Millimeters * 100, Units.Millimeters * 5, "AVG Service Times:", 8, FontStyle.Bold);
+        dataBand.Height += Units.Millimeters * 5;
+
+        Dictionary<string, double> averageServiceTimes = FindAverageServiceTimePerServiceProvider();
+        if (averageServiceTimes.Count == 0)
+        {
+           
+            DisplayTextObject(dataBand, "NoFinishedTicketsMessage", 0, dataBand.Height, Units.Millimeters * 100, Units.Millimeters * 5, "No finished tickets at the moment");
+            dataBand.Height += Units.Millimeters * 10;
+        }
+        else
+        {
+            foreach (var kvp in averageServiceTimes)
             {
-                string servicePointName = kvp.Key;
-                double avgWaitTimeSeconds = kvp.Value;
+                string serviceProviderName = kvp.Key;
+                double avgServiceTimeSeconds = kvp.Value;
 
-                int minutes = (int)avgWaitTimeSeconds / 60;
-                int seconds = (int)avgWaitTimeSeconds % 60;
+                int serviceMinutes = (int)avgServiceTimeSeconds / 60;
+                int serviceSeconds = (int)avgServiceTimeSeconds % 60;
 
-                string formattedAvgWaitTime = $"{minutes} min {seconds} sec";
+                string formattedAvgServiceTime = $"{serviceMinutes} min {serviceSeconds} sec";
 
-                DisplayTextObject(dataBand, $"{servicePointName}Name", 0, dataBand.Height, Units.Millimeters * 50, Units.Millimeters * 5, servicePointName);
-                DisplayTextObject(dataBand, $"{servicePointName}AvgWaitTime", Units.Millimeters * 50, dataBand.Height, Units.Millimeters * 50, Units.Millimeters * 5, formattedAvgWaitTime);
+                DisplayTextObject(dataBand, $"{serviceProviderName}ServiceName", 0, dataBand.Height, Units.Millimeters * 50, Units.Millimeters * 5, serviceProviderName);
+                DisplayTextObject(dataBand, $"{serviceProviderName}AvgServiceTime", Units.Millimeters * 50, dataBand.Height, Units.Millimeters * 50, Units.Millimeters * 5, formattedAvgServiceTime);
 
                 dataBand.Height += Units.Millimeters * 10;
             }
-
-            var result = report.Prepare();
-            _logger.LogInformation("Report prepared: {result}");
-
-            var pdfExport = new PDFExport();
-            MemoryStream memoryStream = new MemoryStream();
-            report.Export(pdfExport, memoryStream);
-
-            var pdfBytes = memoryStream.ToArray();
-            return pdfBytes;
         }
-        catch (Exception ex)
-        {
-            _logger.LogError($"Exception occurred in GenerateReport: {ex.Message}");
-            throw;
-        }
+
+        var result = report.Prepare();
+        _logger.LogInformation("Report prepared: {result}");
+
+        var pdfExport = new PDFExport();
+        MemoryStream memoryStream = new MemoryStream();
+        report.Export(pdfExport, memoryStream);
+
+        var pdfBytes = memoryStream.ToArray();
+        return pdfBytes;
     }
+    catch (Exception ex)
+    {
+        _logger.LogError($"Exception occurred in GenerateReport: {ex.Message}");
+        throw;
+    }
+}
+
 
     private void DisplayTextObject(DataBand dataBand, string name, float x, float y, float width, float height, string text, float fontSize = 0, FontStyle fontStyle = FontStyle.Regular)
     {
@@ -119,7 +150,7 @@ public class ReportService : IReportService
         {
             connection.Open();
 
-            using (var command = new NpgsqlCommand("SELECT ServicePoint, AVG(EXTRACT(EPOCH FROM (ServiceTime - IssueTime))) AS AvgWaitTime " +
+            using (var command = new NpgsqlCommand("SELECT ServicePoint, AVG(EXTRACT(EPOCH FROM (ServiceStartTime - IssueTime))) AS AvgWaitTime " +
                                                    "FROM Ticket " +
                                                    "WHERE Status = 'Finished' " +
                                                    "GROUP BY ServicePoint", connection))
@@ -138,5 +169,34 @@ public class ReportService : IReportService
         }
 
         return averageWaitingTimes;
+    }
+
+    private Dictionary<string, double> FindAverageServiceTimePerServiceProvider()
+    {
+        Dictionary<string, double> averageServiceTimes = new Dictionary<string, double>();
+
+        using (var connection = new NpgsqlConnection(_connectionString))
+        {
+            connection.Open();
+
+            using (var command = new NpgsqlCommand("SELECT ServiceProvider, AVG(EXTRACT(EPOCH FROM (ServiceCompleteTime - ServiceStartTime))) AS AvgServiceTime " +
+                                                   "FROM Ticket " +
+                                                   "WHERE Status = 'Finished' " +
+                                                   "GROUP BY ServiceProvider", connection))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string serviceProvider = reader["ServiceProvider"].ToString();
+                        double avgServiceTime = Convert.ToDouble(reader["AvgServiceTime"]);
+
+                        averageServiceTimes.Add(serviceProvider, avgServiceTime);
+                    }
+                }
+            }
+        }
+
+        return averageServiceTimes;
     }
 }
