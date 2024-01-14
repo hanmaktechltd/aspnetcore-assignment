1cfb45282d820486626dfe47c6d36f8cbad0c448
danielmwaveges <danielmwaveges@gmail.com>
2024-01-14 10:31:09 +0300

    Implement ServiceModel, ServicesViewModel and CheckinPage view.

---
+++ Queue Management System/Queue Management System/Models/ServiceModel.cs
@@ -0,0 +1,11 @@
+namespace Queue_Management_System.Models
+{
+    public class ServiceModel
+    {
+        public string ServiceId { get; set; }
+    
+        public string ServiceDescription { get; set; }
+
+    }
+    
+}
---
+++ Queue Management System/Queue Management System/Models/ServicesViewModel.cs
@@ -0,0 +1,7 @@
+namespace Queue_Management_System.Models
+{
+    public class ServicesViewModel
+    {
+        public ServiceModel[] Services {get; set;}
+    }
+}
