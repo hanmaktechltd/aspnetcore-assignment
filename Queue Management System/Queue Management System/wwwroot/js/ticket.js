a4cf4510c54537623155d86ed1d8351fb2e39ddd
23po <ckodalo@gmail.com>
2024-01-21 05:27:05 +0300

    Adds Text to Speech

--- Queue Management System/Queue Management System/wwwroot/js/ticket.js
+++ Queue Management System/Queue Management System/wwwroot/js/ticket.js
@@ -3,9 +3,23 @@ const hubConnection = new signalR.HubConnectionBuilder()
     .build();
 
 hubConnection.on("ReceiveCalledTicket", function (ticketId, servicePoint) {
+
+   
     $("#calledTicketSection").html(`<p>Called Ticket Number: ${ticketId}</p>
     <p>Service Point: ${servicePoint}</p>
     `);
+
+    speakTicketAnnouncement(ticketId, servicePoint); 
 });
 
 hubConnection.start();
+
+function speakTicketAnnouncement(ticketId, servicePoint) {
+
+    const textToSpeak = `Ticket number ${ticketId} please proceed to ${servicePoint}`;
+    const utterance = new SpeechSynthesisUtterance(textToSpeak);
+
+    utterance.rate = 0.8;
+
+    window.speechSynthesis.speak(utterance);
+}
