8ee0c50965e93c863b98861eb52f7bf0b9d9bf3a
danielmwaveges <danielmwaveges@gmail.com>
2024-01-23 07:59:24 +0300

    Implement ServicePoint and NotConfiguredServicePoint Pages

--- Queue Management System/Queue Management System/Models/ServicePointOperationViewModel.cs
+++ Queue Management System/Queue Management System/Models/ServicePointOperationViewModel.cs
@@ -6,9 +6,9 @@ namespace Queue_Management_System.Models
 
         public string? ServiceDescription {get; set;}
 
-        public TicketModel[]? TicketsInQueue {get; set;}
+        public IEnumerable<TicketModel>? TicketsInQueue {get; set;}
 
-        public TicketModel[]? NoshowTickets {get; set;}
+        public IEnumerable<TicketModel>? NoShowTickets {get; set;}
 
         public IEnumerable<ServiceModel>? Services {get; set;}
 
