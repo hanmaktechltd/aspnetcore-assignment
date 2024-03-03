81b2672c4598e516140b57d0758fa6c3a13a8751
Amos Ndonga <amosndonga@gmail.com>
2024-03-03 09:35:10 +0300

    Set Up Models
    Created Ticket Model, and associated servicepoints and serviceprovider enums

---
+++ Queue Management System/Queue Management System/Models/Ticket.cs
@@ -0,0 +1,12 @@
+﻿namespace Queue_Management_System.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public int CustomerNumber { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public ServicePointEnum ServicePointEnum  { get; set; }
+        public  ServiceProviderEnum ServiceProviderEnum { get; set; }
+        public int WaitingTimeInMinutes { get; set; }
+    }
+}
