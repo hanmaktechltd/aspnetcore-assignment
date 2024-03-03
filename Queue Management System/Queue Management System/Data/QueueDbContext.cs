a235dc04c5fb23b77aecd9980af2ba362400d3eb
Hillary Kemboi <kemboihillz@gmail.com>
2024-03-03 06:03:57 +0300

    created DbContext

---
+++ Queue Management System/Queue Management System/Data/QueueDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Queue_Management_System.Data
+{
+    public class QueueDbContext:DbContext
+    {
+        public QueueDbContext(DbContextOptions<QueueDbContext> options) : base(options)
+        {
+                
+        }
+    }
+}
