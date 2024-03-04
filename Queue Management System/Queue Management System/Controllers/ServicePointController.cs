b1341d81af440ddb8d8154712fabe0f45b1f7191
korirnehemia0 <korirnehemia0@gmail.com>
2024-03-04 11:33:47 +0300

    Configured service point

---
+++ Queue Management System/Queue Management System/Controllers/ServicePointController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Queue_Management_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicePointController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ServicePointController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}/next")]
+        public IActionResult GetNextNumber(int id)
+        {
+            var nextNumber = _context.QueueItems
+                .Where(q => q.ServicePointId == id)
+                .OrderBy(q => q.Id)
+                .Select(q => q.TicketNumber)
+                .FirstOrDefault();
+
+            if (nextNumber == null)
+                return NotFound();
+
+            return Ok(nextNumber);
+        }
+    }
+}
---
+++ Queue Management System/Queue Management System/Models/AppDbContext .cs
@@ -0,0 +1,22 @@
+﻿using FastReport.AdvMatrix;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Queue_Management_System.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<ServicePoint> ServicePoints { get; set; }
+        public DbSet<QueueItem> QueueItems { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    
+    }
+}
